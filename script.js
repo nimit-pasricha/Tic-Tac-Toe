8955450bd63302491ac92f89f8bd3171b145c4a1
nimit-pasricha <pasricha.nimit@gmail.com>
2024-06-05 15:02:26 +0400

    Create 2D array to store gameboard in

---
+++ script.js
@@ -0,0 +1,28 @@
+const gameBoard = (function GameBoard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  // create a 2d array which will represent the state
+  // of the game.
+  // eg: [[1,2,3], [4,5,6], [7,8,9]] is equivalent to
+  /*
+    1 2 3
+    4 5 6
+    7 8 9
+  */
+  for (let i = 0; i < rows; i++) {
+    board.push([]);
+    for (let j = 0; j < columns; j++) {
+      board[i][j] = "";
+    }
+  }
+
+  const getBoard = () => board;
+
+  const placeMarker = (row, column, ) => {
+
+  }
+
+  return {board, getBoard};
+})();
