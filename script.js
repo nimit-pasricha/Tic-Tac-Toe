3037ef966cf9392792d262019039c2aedbb7e931
nimit-pasricha <pasricha.nimit@gmail.com>
2024-06-06 15:18:30 +0400

    Code Cleanup

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const gameBoard = function GameBoard() {
   const columns = 3;
   const board = [];
 
-  // create a 2d array which will represent the state
-  // of the game.
-  // eg: [[1,2,3], [4,5,6], [7,8,9]] is equivalent to
+  /* create a 2d array which will represent the state
+     of the game.
+     eg: [[1,2,3], [4,5,6], [7,8,9]] is equivalent to */
   /*
     1 2 3
     4 5 6
@@ -26,24 +26,39 @@ const gameBoard = function GameBoard() {
     if (board[row][column] === "-") {
       board[row][column] = token;
     }
-  }
+  };
 
   // PURELY FOR TESTING PURPOSES IN CONSOLE
   const printBoard = () => {
-    console.log(board[0][0] + " " + board[0][1] + " " + board[0][2] + "\n"
-      + board[1][0] + " " + board[1][1] + " " + board[1][2] + "\n"
-      + board[2][0] + " " + board[2][1] + " " + board[2][2]
+    console.log(
+      board[0][0] +
+        " " +
+        board[0][1] +
+        " " +
+        board[0][2] +
+        "\n" +
+        board[1][0] +
+        " " +
+        board[1][1] +
+        " " +
+        board[1][2] +
+        "\n" +
+        board[2][0] +
+        " " +
+        board[2][1] +
+        " " +
+        board[2][2]
     );
-  }
+  };
 
-  return {board, getBoard, placeMarker, printBoard};
+  return { board, getBoard, placeMarker, printBoard };
 };
 
-const gameController = function() {
+const gameController = function () {
   const playerTokens = {
     player1: "X",
-    player2: "O"
-  }
+    player2: "O",
+  };
 
   let currentPlayer = "player1";
 
@@ -57,19 +72,18 @@ const gameController = function() {
       console.log(checkForVictory());
       switchPlayerTurn();
     }
-  }
+  };
 
   function switchPlayerTurn() {
     currentPlayer = currentPlayer === "player1" ? "player2" : "player1";
   }
 
-  const allEqual = (arr) => arr.every(v => v === arr[0]);
+  const allEqual = (arr) => arr.every((v) => v === arr[0]);
   const currentBoard = board.getBoard();
 
-    // check for winning conditions
-    // returns nothing (undefined) if no one has won yet
+  // check for winning conditions
+  // returns nothing (undefined) if no one has won yet
   function checkForVictory() {
-
     // check if any row has three consecutive symbols
     for (let i of currentBoard) {
       if (allEqual(i)) {
@@ -80,23 +94,27 @@ const gameController = function() {
 
     // check if any of the columns have 3 consecutive symbols
     for (let i = 0; i < 3; i++) {
-      if (currentBoard[0][i] === currentBoard[1][i]
-        && currentBoard[1][i] === currentBoard[2][i]) {
-          if (currentBoard[0][i] === "X") return "Player 1 Wins";
-          if (currentBoard[0][i] === "O") return "Player 2 Wins";
+      if (
+        currentBoard[0][i] === currentBoard[1][i] &&
+        currentBoard[1][i] === currentBoard[2][i]
+      ) {
+        if (currentBoard[0][i] === "X") return "Player 1 Wins";
+        if (currentBoard[0][i] === "O") return "Player 2 Wins";
       }
     }
 
     // Check for matching diagonals
-    if (currentBoard[0][0] === currentBoard[1][1]
-      && currentBoard[1][1] === currentBoard[2][2]
+    if (
+      currentBoard[0][0] === currentBoard[1][1] &&
+      currentBoard[1][1] === currentBoard[2][2]
     ) {
       if (currentBoard[0][0] === "X") return "Player 1 Wins";
       if (currentBoard[0][0] === "O") return "Player 2 Wins";
     }
 
-    if (currentBoard[0][2] === currentBoard[1][1]
-      && currentBoard[1][1] === currentBoard[2][0]
+    if (
+      currentBoard[0][2] === currentBoard[1][1] &&
+      currentBoard[1][1] === currentBoard[2][0]
     ) {
       if (currentBoard[0][2] === "X") return "Player 1 Wins";
       if (currentBoard[0][2] === "O") return "Player 2 Wins";
@@ -105,19 +123,20 @@ const gameController = function() {
 
   const getBoard = board.getBoard;
 
-  
-  return {playRound, checkForVictory, getBoard}
+  return { playRound, checkForVictory, getBoard };
 };
 
-const displayController = (function() {
+const displayController = (function () {
   const game = gameController();
 
+  // Add the correct symbol to the correct position in the array
+  // based on which square is clicked and whose turn it is
   const gameSlots = document.querySelectorAll(".game-slot");
-  gameSlots.forEach(gameSlot => {
+  gameSlots.forEach((gameSlot) => {
     gameSlot.addEventListener("click", () => {
       const row = gameSlot.getAttribute("data-row");
       const column = gameSlot.getAttribute("data-column");
       game.playRound(row, column);
     });
-  })
-}());
+  });
+})();
